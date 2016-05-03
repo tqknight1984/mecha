bbf587189964a08a0a50f8067f496e76d5dde3ed
tqknight1984 <32050491@qq.com>
2016-05-03 11:35:53 +0800

    20160503

--- game-server/app/util/dispatcher.js
+++ game-server/app/util/dispatcher.js
@@ -1,6 +1,6 @@
 var crc = require('crc');
 
-module.exports.dispatch = function(uid, connectors) {
-	var index = Math.abs(crc.crc32(uid)) % connectors.length;
-	return connectors[index];
+module.exports.dispatch = function(uid, servers) {
+	var index = Math.abs(crc.crc32(uid)) % servers.length;
+	return servers[index];
 };
